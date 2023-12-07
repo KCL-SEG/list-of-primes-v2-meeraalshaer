797026af093b1063ef5713ae2b1bcf92ce5aa642
Meera Alshaer <miraalshaer.m@gmail.com>
2023-12-07 18:54:19 +0000

    Implement prime number generation

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer.")
+
+    primes_list = []
+    num = 2
+
+    while len(primes_list) < number_of_primes:
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes_list.append(num)
+        num += 1
+
+    return primes_list
